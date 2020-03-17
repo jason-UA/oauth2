0483e572620d5ae10407435ddf366199e8a399d6
jason <Jason.Wang2@harman.com>
2020-03-17 16:08:45 +0800

    login token

--- src/main/java/com/example/oauth2/security/config/ResourceServerConfig.java
+++ src/main/java/com/example/oauth2/security/config/ResourceServerConfig.java
@@ -1,9 +1,35 @@
 package com.example.oauth2.security.config;
 
+import com.example.oauth2.security.handler.MyAuthenticationFailureHandler;
+import com.example.oauth2.security.handler.MyAuthenticationSuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private MyAuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Autowired
+    private MyAuthenticationFailureHandler authenticationFailureHandler;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginProcessingUrl("/login")
+                .successHandler(authenticationSuccessHandler)
+                .failureHandler(authenticationFailureHandler)
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf()
+                .disable();
+    }
 }
---
+++ src/main/java/com/example/oauth2/security/handler/MyAuthenticationSuccessHandler.java
@@ -0,0 +1,90 @@
+package com.example.oauth2.security.handler;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestHandler;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.HashMap;
+
+@Component
+@Slf4j
+@Data
+public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Autowired
+    private AuthorizationServerTokenServices authorizationServerTokenServices;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+        String header = httpServletRequest.getHeader("Authorization");
+        if (header == null || !header.startsWith("Basic ")) {
+            throw new UnapprovedClientAuthenticationException("请求头中无client信息");
+        }
+
+        String[] tokens = this.extractAndDecodeHeader(header, httpServletRequest);
+        String clientId = tokens[0];
+        String clientSecret = tokens[1];
+
+        TokenRequest tokenRequest = null;
+
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+
+        if (clientDetails == null) {
+            throw new UnapprovedClientAuthenticationException("clientId:" + clientId + "对应的信息不存在");
+        } else if (!StringUtils.equals(clientDetails.getClientSecret(), clientSecret)) {
+            throw new UnapprovedClientAuthenticationException("clientSecret不正确");
+        } else {
+            tokenRequest = new TokenRequest(new HashMap<>(), clientId, clientDetails.getScope(), "custom");
+        }
+
+        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+        OAuth2Authentication auth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+
+        OAuth2AccessToken token = authorizationServerTokenServices.createAccessToken(auth2Authentication);
+
+        log.info("登录成功");
+        httpServletResponse.setContentType("application/json;charset=UTF-8");
+        httpServletResponse.getWriter().write(new ObjectMapper().writeValueAsString(token));
+
+    }
+
+    private String[] extractAndDecodeHeader(String header, HttpServletRequest request) {
+        byte[] base64Token = header.substring(6).getBytes(StandardCharsets.UTF_8);
+
+        byte[] decoded;
+        try {
+            decoded = Base64.getDecoder().decode(base64Token);
+        } catch (IllegalArgumentException var7) {
+            throw new BadCredentialsException("Failed to decode basic authentication token");
+        }
+
+        String token = new String(decoded, StandardCharsets.UTF_8);
+        int delim = token.indexOf(":");
+        if (delim == -1) {
+            throw new BadCredentialsException("Invalid basic authentication token");
+        } else {
+            return new String[]{token.substring(0, delim), token.substring(delim + 1)};
+        }
+    }
+}
